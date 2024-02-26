273b90f3af6bf9b92dab4c6b82c6baeab7aad701
AnishHazra <anishhazra108@gmail.com>
2024-02-26 21:54:29 +0530

    fix img in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./About.css";
+import about1 from "../../assets/about1.png"
+import about2 from "../../assets/about2.png"
+import about3 from "../../assets/about3.png"
+
 
 function About() {
   return (
@@ -9,7 +13,7 @@ function About() {
         <div className="about-wpr">
           <div>
             <h2>How we work ?</h2>
-            <img src="./src/assets/about1.png" alt="about1" />
+            <img src={about1} alt="about1" />
             <p>
               Codechef ADGITM chapter organises regular contests , hackathons ,
               weekly and monthly meets , sessions with mentors and seniors , all
@@ -19,7 +23,7 @@ function About() {
           </div>
           <div>
             <h2>Our Goal</h2>
-            <img src="./src/assets/about2.png" alt="about2" />
+            <img src={about2} alt="about2" />
             <p>
               To provide students with all the mentorship, resources, and
               opportunities they need to develop top-notch programming skills
@@ -28,7 +32,7 @@ function About() {
           </div>
           <div>
             <h2>Why Should you Join Us?</h2>
-            <img src="./src/assets/about3.png" alt="about3" />
+            <img src={about3} alt="about3" />
             <p>
               You learn many things, like how to work with a team, how to manage
               the schedule along with your studies. So you learn how to talk to
